ed515c6e5fb16e33fc2d829f553723d033606eca
Jesse Luo <radishchrist@gmail.com>
2014-05-07 23:32:23 +0800

    Rendering Less Templates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var serveJade = require('./lib/processor/jade');
+var serveLess = require('./lib/processor/less');
 
 function miniharp(root) {
   var app = connect();
@@ -10,6 +11,7 @@ function miniharp(root) {
   // should serve /bar.html if bar.html exists
   app.use(serveStatic(root));
   app.use(serveJade(root));
+  app.use(serveLess(root));
 
   return app;
 }
